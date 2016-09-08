3b0d8b24adddaa3df9466073fb61b95e98da3e1b
jun159 <baojun159@gmail.com>
2016-09-08 13:03:15 +0800

    Added new test, fail build

--- src/test/java/test/CircularShiftTest.java
+++ src/test/java/test/CircularShiftTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import CS3213.CircularShift;
+import CS3213.RequiredWords;
 
 import java.util.HashSet;
 
@@ -23,5 +24,25 @@ public class CircularShiftTest {
         assertTrue(testSet.contains("This Circular Shift Test"));
         assertTrue(testSet.contains("Circular Shift Test This"));
         assertTrue(testSet.contains("Shift Test This Circular"));
+    }
+    
+    @Test
+    public void testFilterCircularShifts() throws Exception {
+    	RequiredWords requiredWords = RequiredWords.getRequiredWords();
+    	requiredWords.addRequiredWords("Test");
+    	requiredWords.addRequiredWords("THIs");
+    	requiredWords.addRequiredWords("Hello");
+    	
+        CircularShift circularShift = new CircularShift("tEst this Circular shIft");
+        String[] shifts = circularShift.getCircularShifts();
+        HashSet<String> testSet = new HashSet<String>();
+        for (String str : shifts) {
+            testSet.add(str);
+        }
+        assertTrue(testSet.size() == 2);
+        assertTrue(testSet.contains("Test This Circular Shift"));
+        assertTrue(testSet.contains("This Circular Shift Test"));
+        assertFalse(testSet.contains("Circular Shift Test This"));
+        assertFalse(testSet.contains("Shift Test This Circular"));
     }
 }
